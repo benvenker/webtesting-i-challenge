42fce4503262269656359d0b8d7005200a3f2572
Ben Venker <bvemails@gmail.com>
2020-05-01 11:11:18 -0500

    first test passing

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,12 @@
-const enhancer = require('./enhancer.js');
+const { repair } = require("./enhancer.js");
 // test away!
+
+describe("enhancement functions", () => {
+  describe("repair()", () => {
+    it("should return an item with durability: 100", () => {
+      const item = { name: "sword", durability: 50 };
+
+      expect(repair(item)).toEqual({ name: "sword", durability: 100 });
+    });
+  });
+});
